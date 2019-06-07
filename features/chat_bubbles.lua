b3d1de68b8cdf12e1ee042968a1e7dacd139fc6c
Major Sprite <factorio.github@gmail.com>
2019-06-07 17:19:34 +0200

    added chat bubbles

---
+++ features/chat_bubbles.lua
@@ -0,0 +1,36 @@
+local Event = require "utils.event"
+local Global = require "utils.global"
+local Validate = require "utils.validate"
+
+local current_messages = {}
+
+Global.register({ current_messages = current_messages }, function(global)
+  current_messages = global.current_messages
+end)
+
+
+local function chat(event)
+  local player = game.players[event.player_index]
+  if not Validate:player(player) then return end
+  local surface = player.surface
+  local message = event.message
+
+  if message then
+     
+    if current_messages[player.name] then
+      rendering.destroy(current_messages[player.name])
+    end
+    local id = rendering.draw_text({
+      text = message, 
+      surface = surface,
+      target = player.character,
+      target_offset = { 0, -2.2 },
+      color = player.color,
+      time_to_live = 120,
+      alignment = "center"
+    })
+    current_messages[player.name] = id
+  end
+end
+
+Event.register("console_chat", chat)
