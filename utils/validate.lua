929457a1b15ab137a3442b8d7214460a6bc2564a
Major Sprite <factorio.github@gmail.com>
2019-06-09 11:34:54 +0200

    Minor changes to the validates

--- utils/validate.lua
+++ utils/validate.lua
@@ -1,7 +1,7 @@
 local Validate = {}
 
 
-function Validate:player(player) 
+function Validate.player(player) 
   if not player then return false end
   if not player.valid then return false end
   if not player.character then return false end
@@ -10,10 +10,14 @@ function Validate:player(player)
   return true
 end
 
-function Validate:element(element) 
+function Validate.element(element) 
+  if not element then return false end
+  if not element.valid then return false end
+
+  return true
 end
 
-function Validate:entity(entity) 
+function Validate.entity(entity) 
   if not entity then return false end
   if not entity.valid then return false end
   return true
